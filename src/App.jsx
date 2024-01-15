d548f6afd8558348bd13492e2275dc52bd9431b8
Aveiro <matheusaveirofagundes@gmail.com>
2024-01-15 15:18:58 -0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+
+import Perfil from "./components/Perfil";
+import Formulario from "./components/Formulario";
+import ReposList from "./components/ReposList";
+
+function App() {
+  const [formularioEstaVisivel, setFormularioEstaVisivel] = useState(true);
+  const [nomeUsuario, setNomeUsuario] = useState('');
+
+  return (
+    <>
+      <input type="text" onBlur={(e) => setNomeUsuario(e.target.value)} />
+
+      {nomeUsuario.length > 4 && (
+        <>
+          <Perfil nomeUsuario={nomeUsuario} />
+          <ReposList nomeUsuario={nomeUsuario} />
+        </>
+      )}
+
+      {/* {formularioEstaVisivel && ( //mount /  onmount
+        <Formulario />
+      )}
+
+      <button onClick={() => setFormularioEstaVisivel(!formularioEstaVisivel)} type="button">toggle form</button> */}
+    </>
+  )
+}
+
+export default App
+
+/* ESTUDOS
+<h1>Olá, {pessoa.nome}</h1>
+<h2>Subtitulo</h2>
+{estaDeDia === true ? 'Bom dia' : 'Boa noite'}
+{estaDeDia ? 'Bom dia' : 'Boa noite'}
+{estaDeDia && 'Bom dia'} */
